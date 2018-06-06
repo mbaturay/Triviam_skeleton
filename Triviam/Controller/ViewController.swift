28ab7cdeda1df2695d8a656d69160295bd3ca506
Murat Baturay <murat.baturay@gmail.com>
2018-06-05 17:57:33 -0700

    Building
    Still small problems

--- Triviam/Controller/ViewController.swift
+++ Triviam/Controller/ViewController.swift
@@ -11,7 +11,14 @@ import SwiftyJSON
 import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    let URL     = "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean"
+    var items   : [DataModel] = []
+    var counter : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +27,64 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-
+    func getQuestions(url: String) {
+        
+        Alamofire.request(url).responseJSON {
+            response in
+            if response.result.isSuccess {
+                let json : JSON = JSON(response.result.value!)
+                self.populateMyQuestionsArray(json: json)
+            }
+            else {
+                print("Fail!")
+            }
+        }
+    }
+    
+    func populateMyQuestionsArray (json : JSON) {
+        
+        for index in 0...9 {
+            items.append(DataModel(
+                question:       json["results"][index]["question"].stringValue,
+                correct_answer: json["results"][index]["correct_answer"].stringValue))
+        }
+        
+    }
+    
+    @IBAction func answerButton(_ sender: UIButton) {
+        let question : String   = items[counter].question
+        let _   : String   = items[counter].correct_answer
+        
+        questionLabel.text = question.htmlToString
+        
+        
+        if counter < 9 {
+            counter = counter + 1
+        }
+        else {
+            counter = 0
+        }    }
+    
+    
 }
+/* End of Main
+   *************************/
 
+
+
+
+/* E X T E N S I O N */
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [NSAttributedString.DocumentReadingOptionKey.documentType:  NSAttributedString.DocumentType.html], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
